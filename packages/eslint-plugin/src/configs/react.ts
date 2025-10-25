28502f76175037c681cb32db9d20ffb3d8b64cc5
Davide Di Criscito <davidedc@outlook.com>
2025-10-25 22:47:48 +0200

    fix(eslint-plugin): add missing plugins, configs, and rules
    provide same plugins, configs, and rules applied in the js/ts config

--- packages/eslint-plugin/src/configs/react.ts
+++ packages/eslint-plugin/src/configs/react.ts
@@ -1,8 +1,16 @@
 import type { Linter } from 'eslint';
 import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import jsdoc from 'eslint-plugin-jsdoc';
+import nodeDependencies from 'eslint-plugin-node-dependencies';
+import prettierPlugin from 'eslint-plugin-prettier';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unicorn from 'eslint-plugin-unicorn';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 import configIgnores from './ignores.js';
 import configJavascript from './javascript.js';
@@ -16,11 +24,23 @@ const configReact: Linter.Config[] = defineConfig([
   {
     files: ['**/*.{jsx,tsx,mjsx,mtsx}'],
     plugins: {
+      jsdoc,
+      import: importPlugin,
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
       react: reactPlugin,
       // @ts-expect-error react-hooks types not yet provided
       'react-hooks': reactHooks,
     },
     extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      nodeDependencies.configs['flat/recommended'],
+      unicorn.configs.recommended,
+      jsdoc.configs['flat/contents-typescript-flavor'],
+      jsdoc.configs['flat/logical-typescript-flavor'],
+      jsdoc.configs['flat/requirements-typescript-flavor'],
+      jsdoc.configs['flat/stylistic-typescript-flavor'],
       reactPlugin.configs.flat.recommended,
       reactPlugin.configs.flat['jsx-runtime'],
       // @ts-expect-error react-hooks types not yet provided
